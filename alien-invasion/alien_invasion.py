1c49d1129ac56901aceed6fbe2e475d16157ad8c
Sonal Agrawal <sonalsinghal755@gmail.com>
2019-06-07 17:14:52 +0530

    delete old bullets

--- alien-invasion/alien_invasion.py
+++ alien-invasion/alien_invasion.py
@@ -23,6 +23,12 @@ def run_game():
 		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
 		bullets.update()
+
+		# get rid of bullets that have disappeared
+		for bullet in bullets.copy():
+			if bullet.rect.bottom <= 0:
+				bullets.remove(bullet)
+	
 		gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
