8784fc9d47a680f9318173e729070be7c26b1e2d
Sonal Agrawal <sonalsinghal755@gmail.com>
2019-06-07 13:56:24 +0530

    continous movement

--- alien-invasion/game_functions.py
+++ alien-invasion/game_functions.py
@@ -9,11 +9,16 @@ def check_events(ship):
 
 			elif event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_RIGHT:
-					# move the ship to the right
-					ship.rect.centerx += 1
-				if event.key == pygame.K_LEFT:
-					# move the ship to the right
-					ship.rect.centerx -= 1
+					ship.moving_right = True
+				elif event.key == pygame.K_LEFT:
+					ship.moving_left = True
+
+
+			elif event.type == pygame.KEYUP:
+				if event.key == pygame.K_RIGHT:
+					ship.moving_right = False				
+				elif event.key == pygame.K_LEFT:
+					ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
 	"""Update images on the screen and flip to a new screen."""
--- alien-invasion/ship.py
+++ alien-invasion/ship.py
@@ -14,5 +14,16 @@ class Ship():
 		self.rect.centerx = self.screen_rect.centerx
 		self.rect.bottom = self.screen_rect.bottom
 
+		# movement flag
+		self.moving_right = False
+		self.moving_left = False
+
+	def update(self):
+		"""Update ships position based on movement flag."""
+		if self.moving_right:
+			self.rect.centerx += 1
+		if self.moving_left:
+			self.rect.centerx -= 1
+
 	def blitme(self):
 		self.screen.blit(self.image, self.rect)
