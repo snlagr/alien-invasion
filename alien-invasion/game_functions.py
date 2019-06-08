563df198ab39e1e3bf5b2cd589fa37b5b6b07bb5
Sonal Agrawal <sonalsinghal755@gmail.com>
2019-06-09 00:00:44 +0530

    reset if alien reach screen bottom

--- alien-invasion/game_functions.py
+++ alien-invasion/game_functions.py
@@ -149,6 +149,15 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
 	# pause
 	sleep(0.5)
 
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+	"""check if aliens have reached bottom of the screen"""
+	screen_rect = screen.get_rect()
+	for alien in aliens.sprites():
+		if alien.rect.bottom >= screen_rect.bottom:
+			# treat this same as ship got hit
+			ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+			break
+
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
 	"""Update the position of all aliens in the fleet."""
 	check_fleet_edges(ai_settings, aliens)
@@ -157,3 +166,6 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
 	# look for alien-ship collisions
 	if pygame.sprite.spritecollideany(ship, aliens):
 		ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+
+	# look for aliens hitting bottom of the screen
+	check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
