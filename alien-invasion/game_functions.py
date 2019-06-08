8b61d39380c7df1c542be87bbb10a097646d3f14
Sonal Agrawal <sonalsinghal755@gmail.com>
2019-06-08 13:30:34 +0530

    press q to quit

--- alien-invasion/game_functions.py
+++ alien-invasion/game_functions.py
@@ -28,6 +28,8 @@ def check_keydown_events(event, ai_settings, screen,
 		ship.moving_left = True
 	elif event.key == pygame.K_SPACE:
 		fire_bullet(ai_settings, screen, ship, bullets)
+	elif event.key == pygame.K_q:
+		sys.exit()
 		
 
 def fire_bullet(ai_settings, screen, ship, bullets):
