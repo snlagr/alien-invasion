7dda442e1c5dba30ee207024d6ea34df2926d813
Sonal Agrawal <sonalsinghal755@gmail.com>
2019-06-10 18:15:56 +0530

    visualize results histogram using pygal

--- data-vis/die_visual.py
+++ data-vis/die_visual.py
@@ -1,3 +1,4 @@
+import pygal
 from die import Die
 
 # create a D6
@@ -15,4 +16,13 @@ for value in range(1, die.num_sides+1):
 	frequency = results.count(value)
 	frequencies.append(frequency)
 
-print(frequencies)
+# visualize the results
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
