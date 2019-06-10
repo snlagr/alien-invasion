2b5d7bddd1126fef87229c51cf375125663cfc94
Sonal Agrawal <sonalsinghal755@gmail.com>
2019-06-10 17:03:53 +0530

    added option to generate multiple walks

--- data-vis/rw_visual.py
+++ data-vis/rw_visual.py
@@ -2,9 +2,14 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# make a random walk, and plot the points
-rw = RandomWalk()
-rw.fill_walk()
+# keep making new plots as long as program is active
+while True: 
+	# make a random walk, and plot the points
+	rw = RandomWalk()
+	rw.fill_walk()
+	plt.scatter(rw.x_values, rw.y_values, s=15)
+	plt.show()
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+	keep_running = input("Make another walk? (y/n): ")
+	if keep_running == 'n':
+		break
