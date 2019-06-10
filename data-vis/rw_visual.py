5c6e01c6b1a3d7e325ca7168ce08db37e2f7bd86
Sonal Agrawal <sonalsinghal755@gmail.com>
2019-06-10 17:25:21 +0530

    rw shows start & end points

--- data-vis/rw_visual.py
+++ data-vis/rw_visual.py
@@ -10,6 +10,11 @@ while True:
 
 	point_numbers = list(range(rw.num_points))
 	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+		
+	# Emphasize first and last points
+	plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+	plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
 	plt.show()
 
 	keep_running = input("Make another walk? (y/n): ")
