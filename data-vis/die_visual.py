d129259ad98a0262b2995a65e8950b6c83a33508
Sonal Agrawal <sonalsinghal755@gmail.com>
2019-06-10 18:08:50 +0530

    added frequencies in die

--- data-vis/die_visual.py
+++ data-vis/die_visual.py
@@ -5,8 +5,14 @@ die = Die()
 
 # make some rolls and store results in a list
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
 	result = die.roll()
 	results.append(result)
 
-print(results)
+# analyze the results
+frequencies = []
+for value in range(1, die.num_sides+1):
+	frequency = results.count(value)
+	frequencies.append(frequency)
+
+print(frequencies)
